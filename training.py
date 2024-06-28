6e972a41ee629f4d471c93647d40e2a6c71c3442
ViktorDikarev34 <ViktorDikarev1992@yandex.ru>
2024-06-28 14:37:39 +0300

    training

--- training.py
+++ training.py
@@ -1,100 +1,34 @@
-import asyncio
-import logging
-import random
-import operator
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from aiogram.filters import CommandStart
 from config_reader import config
-from aiogram import html, F
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.utils.formatting import Text, Bold
-from aiogram_dialog.widgets.kbd import Multiselect, Radio
-from aiogram_dialog.widgets.text import Format
-from aiogram.filters.state import StatesGroup, State
-from aiogram.types import CallbackQuery
-from aiogram.types import CallbackQuery, ContentType, Message
-from aiogram_dialog import Window, Dialog, DialogManager, StartMode, setup_dialogs
-from aiogram_dialog.widgets.kbd import Button, Back
-from aiogram_dialog.widgets.text import Const, Format
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from contextlib import suppress
-from aiogram.exceptions import TelegramBadRequest
+import requests
+import time
 
+API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = config.bot_token.get_secret_value()
+TEXT = 'Как запилить из кофига токен'
+ERROR_TEXT = 'здесь должна была быть картинка с усатым котом'
+MAX_COUNTER = 100
 
-#Включаем логирование, чтобы не пропустить важные сообщения
-logging.basicConfig(level=logging.INFO)
-# Объект бота
-bot = Bot(
-    token=config.bot_token.get_secret_value(),
-    default=DefaultBotProperties(
-        parse_mode=ParseMode.HTML)
-    )
+offset = -2
+counter = 0
+chat_id: int
+cat_response: requests.Response
+cat_link: str
 
-# Диспетчер
-dp = Dispatcher()
+while counter < MAX_COUNTER:
+    print('attempt =', counter)
 
-# Запуск процесса поллинга новых апдейтов
-async def main():
-    await dp.start_polling(bot)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
-
-
-
-user_data = {}
-
-def get_keyboard():
-    buttons = [
-        [
-            types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
-            types.InlineKeyboardButton(text="+1", callback_data="num_incr")
-        ],
-        [types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")]
-    ]
-    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
-    return keyboard
-
-
-async def update_num_text(message: types.Message, new_value: int):
-    await message.edit_text(
-        f"Укажите число: {new_value}",
-        reply_markup=get_keyboard()
-    )
-
-
-@dp.message(Command("numbers"))
-async def cmd_numbers(message: types.Message):
-    user_data[message.from_user.id] = 0
-    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
-
-
-@dp.callback_query(F.data.startswith("num_"))
-async def callbacks_num(callback: types.CallbackQuery):
-    user_value = user_data.get(callback.from_user.id, 0)
-    action = callback.data.split("_")[1]
-
-    if action == "incr":
-        user_data[callback.from_user.id] = user_value+1
-        await update_num_text(callback.message, user_value+1)
-    elif action == "decr":
-        user_data[callback.from_user.id] = user_value-1
-        await update_num_text(callback.me￼ 
-
-ssage, user_value-1)
-    elif action == "finish":
-        await callback.message.edit_text(f"Итого: {user_value}")
-
-    await callback.answer()
-
-async def update_num_text(message: types.Message, new_value: int):
-    with suppress(TelegramBadRequest):
-        await message.edit_text(
-            f"Укажите число: {new_value}",
-            reply_markup=get_keyboard()
-        )
-
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&{ERROR_TEXT}')
+    time.sleep(1)
+    counter += 1
