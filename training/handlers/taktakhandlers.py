993b083cc01088853e6fab1ee59e826b86a6534c
ViktorDikarev34 <ViktorDikarev1992@yandex.ru>
2024-07-12 20:35:00 +0300

    press_and_assembly

--- training/handlers/taktakhandlers.py
+++ training/handlers/taktakhandlers.py
@@ -6,6 +6,8 @@ from aiogram.types import Message, CallbackQuery
 
 router= Router()
 
+user_data={}
+
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     keyboard = creat_inlinekb(1, 'платежи', 'заказы')
@@ -16,8 +18,8 @@ async def process_start_command(message: Message):
 
 @router.callback_query(F.data == "платежи")
 async def send_random_value(callback: CallbackQuery):
-    keyboard = creat_inlinekb(1, 'просроченные', 'сегодня', 'на этой неделе', 'в этом месяце', last_btn='подтвердить')
-    await callback.message.answer(
+    keyboard = creat_inlinekb(1, 'просроченные', 'сегодня', 'на этой неделе', 'в этом месяце', 'очистить выбор', 'отменить последний выбор', last_btn='подтвердить')
+    await callback.message.edit_text(
         text='Какой период',
         reply_markup=keyboard
     )
@@ -25,12 +27,40 @@ async def send_random_value(callback: CallbackQuery):
 
 @router.callback_query(F.data == "заказы")
 async def send_random_value(callback: CallbackQuery):
-    keyboard = creat_inlinekb(1, 'просроченные', 'сегодня', 'на этой неделе', 'в этом месяце', last_btn='подтвердить')
-    await callback.message.answer(
+    keyboard = creat_inlinekb(1, 'просроченные', 'сегодня', 'на этой неделе', 'в этом месяце', 'очистить выбор', 'отменить последний выбор', last_btn='подтвердить')
+    await callback.message.edit_text(
         text='Какой период',
         reply_markup=keyboard
     )
 
+assembly = set()
+word = []
 
+@router.callback_query(F.data.in_({'просроченные', 'сегодня', 'на этой неделе', 'в этом месяце'}))
+async def press_and_assembly(callback: CallbackQuery):
+    assembly.add(callback.data)
+    word.append(callback.data)
+    await callback.answer(
+        text=f'вы выбрали "{callback.data}"')
 
+@router.callback_query(F.data == 'отменить последний выбор')
+async def cancel_assembly(callback: CallbackQuery):
+    assembly.discard(word[-1])
+    await callback.answer(
+        text=f'вы убрали "{word[-1]}"')
+
+@router.callback_query(F.data == 'очистить выбор')
+async def clear(callback: CallbackQuery):
+    assembly.clear()
+    await callback.answer(
+        text='Вы очистили список',
+        show_alert=True
+    )
+
+@router.callback_query(F.data == 'last_btn')
+async def press_and_assembly(callback: CallbackQuery):
+    await callback.answer(
+        text=f'вы выбрали {assembly}',
+        show_alert=True
+    )
     await callback.answer()
