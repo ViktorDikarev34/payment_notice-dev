69b6bbca55220e0cac345a71c519ece3dd1b47d5
ViktorDikarev34 <ViktorDikarev1992@yandex.ru>
2024-07-31 22:54:48 +0300

    ms

---
+++ training/ms/ms.py
@@ -0,0 +1,20 @@
+
+import requests
+from pprint import pprint
+
+
+
+url = "https://api.moysklad.ru/api/remap/1.2/entity/counterparty/metadata"
+headers = {
+    "Authorization": "9c10e0dc4b1b681143e68c18a5f647e9b6480216",
+    "Accept-Encoding": "gzip"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    print('успех')
+    pprint(response.json())
+else:
+    # Обработка ошибки, если запрос не удался
+    print(f"Ошибка запроса: {response.status_code} - {response.reason}")
