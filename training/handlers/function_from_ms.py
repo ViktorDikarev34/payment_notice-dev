508f0226eda49638e36cbfe4d01e7eedddd7ea48
ViktorDikarev34 <ViktorDikarev1992@yandex.ru>
2024-08-08 00:51:12 +0300

    funct from ms

--- training/handlers/function_from_ms.py
+++ training/handlers/function_from_ms.py
@@ -1,5 +1,27 @@
-async def get_payment_status():
-    return ['новый', 'не оплачено', 'оплачено']
+import requests
 
-async def get_order_status():
-    return ['новый', 'не оплачено', 'оплачено']
+async def search_nessesary(dictionary: dict, *args: str) -> dict | list | str:
+    key = dictionary
+    for i in args:
+        key = key[i]
+    await key
+
+def list_button(token_ms: str = 'fd381c6be45d058539a1f5638ba484ecdd8df885'):
+    try:
+        url = "https://api.moysklad.ru/api/remap/1.2/entity/paymentout/metadata"
+        headers = {
+            "Authorization": f"{token_ms}",
+            "Accept-Encoding": "gzip"
+        }
+
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            print('успех')
+            states = search_nessesary(response.json(), 'states')
+            kbbutton = [i['name'] for i in states]
+    except Exception as ex:
+            print(ex)
+
+a = list_button()
+print(a)
